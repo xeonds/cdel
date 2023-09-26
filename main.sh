d7efa0523bd9717881c7b92e6b41c0ae55b19cfa
xeonds <xeonds@stu.xidian.edu.cn>
2023-09-26 21:45:22 +0800

    Bug fix: now it's mostly useable

--- main.sh
+++ main.sh
@@ -1,22 +1,8 @@
+# If the dir contains bashrc, launch the sub shell and load it
 function cd_hook() {
-	if [ -f "$1/.bashrc" ]; then
-		bash --init-file "$1/.bashrc"
-		pushd . >/dev/null
-		cd "$1"
-	else
-		cd "$1"
-	fi
-}
-
-function exit_hook() {
-	if [ "$PWD" != "$(dirs -l +0)" ]; then
-		popd >/dev/null
-		cd "$(dirs -l +0)"
-		exit
-	else
-		exit
+    cd "$1"; if [ -f ".bashrc" ] && [ "$(pwd)" != "$(getent passwd $USER | awk -F ':' '{print $6}')" ]; then
+        bash --init-file <(cat /etc/profile ~/.bashrc .bashrc)
 	fi
 }
 
 alias cd='cd_hook'
-alias exit='exit_hook'
