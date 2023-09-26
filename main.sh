f325ea3ffbb4306dcb54e170631597e642df3cd5
xeonds <xeonds@stu.xidian.edu.cn>
2023-09-26 22:29:07 +0800

    Bug fix: cd - and cd without param

--- main.sh
+++ main.sh
@@ -1,8 +1,15 @@
-# If the dir contains bashrc, launch the sub shell and load it
 function cd_hook() {
-    cd "$1"; if [ -f ".bashrc" ] && [ "$(pwd)" != "$(getent passwd $USER | awk -F ':' '{print $6}')" ]; then
-        bash --init-file <(cat /etc/profile ~/.bashrc .bashrc)
+    # The normal cd
+    if [ $# == 0 ]; then
+        cd
+    else
+        cd "$1" 
 	fi
+    # If the dir contains bashrc, launch the sub shell and load it
+    if [ -f ".bashrc" ] && [ "$(pwd)" != "$(getent passwd $USER | awk -F ':' '{print $6}')" ]; then
+        pushd .  > /dev/null
+        bash --init-file <(cat /etc/profile ~/.bashrc .bashrc)
+    fi
 }
 
 alias cd='cd_hook'
